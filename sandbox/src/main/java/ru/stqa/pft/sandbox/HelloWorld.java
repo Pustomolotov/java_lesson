56fdb8caa32dd0fe56b110906466303bfb04979a
Pustomolotov <Pustomolotov_job@mail.ru>
2022-12-14 00:29:52 +0300

    создание класса и функции
    Задание №2: Потренироваться использовать функции, классы, объекты и методы
    
    пункты 1 и 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -10,9 +10,24 @@ public class HelloWorld {
 
 		Rectangle r = new Rectangle(4,6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+		Point p1 = new Point();
+		p1.x = 5;
+		p1.y = 6;
+		Point p2 = new Point();
+		p2.x = 7;
+		p2.y = 8;
+
+		distance(p1,p2);
+
 	}
 
 	public static void hello(String somebody){
 		System.out.println("Hello " + somebody + "!");
+	}
+
+	public static void distance(Point p1, Point p2){
+		double d = Math.sqrt((p2.x - p1.x)*2 + (p2.y - p1.y)*2);
+		System.out.println("Расстояние между двумя точками: " + d);
 	}
 }
