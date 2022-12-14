8324a42cc0395d84f1eb8bae09f1cfdecbe1b731
Pustomolotov <Pustomolotov_job@mail.ru>
2022-12-14 10:18:16 +0300

    убрал пробелы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,17 +1,13 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-
   public double x;
   public double y;
-
-
 
   public Point(double x, double y){
     this.x = x;
     this.y = y;
   }
-
   public double distance(Point p2, Point p1){
 
     return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
