4ec7355ed78f51c923cc8e733d195cb423070d3d
Pustomolotov <Pustomolotov_job@mail.ru>
2023-01-22 20:00:08 +0300

    lesson 5 part 8
    ХЕШИРОВАНИЕ ГРУПП

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -37,8 +37,8 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
+  public int count() {
+    return wd.findElements(By.name("selected[]")).size();
   }
   public void selectGroupById(int id) {
     wd.findElement(By.cssSelector("input[value='" + id +"']")).click();
